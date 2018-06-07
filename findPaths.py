70c16d5a833ecc5e971b9cbcadda6994f55e7dce
rreneker <rreneker@umich.edu>
2018-06-07 14:21:08 -0400

    starting point added

--- findPaths.py
+++ findPaths.py
@@ -3,11 +3,18 @@ import csv
 
 kioskList = []
 
+depot = Kiosk("Starting Depo","1376 W Lake Street" ,41.8851024,-87.6618988)
+
 with open('KioskCoords.csv','rb') as csvfile:
     reader = csv.reader(csvfile,delimiter=',',quotechar='"')
     next(reader)
     for row in reader:
         kioskList.append(Kiosk(row[0],row[1],row[2],row[3]))
+
+routeOne = []
+routeTwo = []
+routeOne.append(depot)
+routeTwo.append(depot)
         
 
 for kiosk in kioskList:
