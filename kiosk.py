6edcb95358b0af97cf05a83f9697c3caa86e5c35
rreneker <rreneker@umich.edu>
2018-06-07 12:02:38 -0400

    kiosk class created

---
+++ kiosk.py
@@ -0,0 +1,10 @@
+class Kiosk():
+    def __init__(self,desc="",addr="",lat=0.0,longt=0.0):
+        self.description = desc
+        self.address = addr
+        self.latitude = lat
+        self.longitude = longt
+        self.visited = False
+    
+    def visit(self):
+        self.visited = True
