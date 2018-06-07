1fea095fb36d09b93022925980d3d7ab8bfc0ff1
rreneker <rreneker@umich.edu>
2018-06-07 12:17:27 -0400

    added reader for csv file

--- findPaths.py
+++ findPaths.py
@@ -1,4 +1,10 @@
 from kiosk import Kiosk
+import csv
+
+with open('KioskCoords.csv','rb') as csvfile:
+    reader = csv.reader(csvfile,delimiter=',',quotechar='"')
+    for row in reader:
+        print ', '.join(row)
 
 kiosks = Kiosk("home","123 Fake Street")
 
