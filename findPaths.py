02485af10e57bbba7efe520c78573e9f146bd838
rreneker <rreneker@umich.edu>
2018-06-08 15:39:34 -0400

    splitting stops into north and south

--- findPaths.py
+++ findPaths.py
@@ -1,7 +1,8 @@
 from kiosk import Kiosk, DeliveryDriver
 import csv
 
-kioskList = []
+NorthKioskList = []
+SouthKioskList = []
 
 depot = Kiosk("Starting Depo","1376 W Lake Street" ,41.8851024,-87.6618988)
 
@@ -9,11 +10,26 @@ with open('KioskCoords.csv','rb') as csvfile:
     reader = csv.reader(csvfile,delimiter=',',quotechar='"')
     next(reader)
     for row in reader:
-        kioskList.append(Kiosk(row[0],row[1],float(row[2]),float(row[3])))
+        if float(row[2]) >= depot.latitude:
+            NorthKioskList.append(Kiosk(row[0],row[1],float(row[2]),float(row[3])))
+        else:
+            SouthKioskList.append(Kiosk(row[0],row[1],float(row[2]),float(row[3])))
+        
 
 routeOne = DeliveryDriver(depot)
 routeTwo = DeliveryDriver(depot)
 
-routeOne.addStop(kioskList[0])
-routeOne.printPath()
+NorthKioskList.sort(key=lambda x: x.latitude, reverse=True)
+SouthKioskList.sort(key=lambda x: x.latitude, reverse=True)
+
+print len(NorthKioskList)
+print len(SouthKioskList)
+
+for stop in NorthKioskList:
+    print stop.description+"|"+str(stop.latitude)+"|"+str(stop.longitude)
+print "----------------"
+for stop in SouthKioskList:
+    print stop.description+"|"+str(stop.latitude)+"|"+str(stop.longitude)
+
+
 
