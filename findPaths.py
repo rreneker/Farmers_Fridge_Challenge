3484936accd38e896051466cd7a19e493947fee4
rreneker <rreneker@umich.edu>
2018-06-10 12:06:37 -0400

    refactor to try different approach

--- findPaths.py
+++ findPaths.py
@@ -1,9 +1,10 @@
 from kiosk import Kiosk, DeliveryDriver
 import csv
 
+
+
 KioskList = []
-NorthKioskList = []
-SouthKioskList = []
+
 
 depot = Kiosk("Starting Depo","1376 W Lake Street" ,41.8851024,-87.6618988)
 
@@ -13,23 +14,13 @@ with open('KioskCoords.csv','rb') as csvfile:
     for row in reader:
         KioskList.append(Kiosk(row[0],row[1],float(row[2]),float(row[3])))
         
+KioskList.append(depot)
 
-routeOne = DeliveryDriver(depot)
-routeTwo = DeliveryDriver(depot)
 
-KioskList.sort(key=lambda x: x.latitude, reverse=True)
 
-NorthKioskList = KioskList[:len(KioskList)/2]
-SouthKioskList = KioskList[len(KioskList)/2:]
 
-print len(NorthKioskList)
-print len(SouthKioskList)
 
-for stop in NorthKioskList:
-    print stop.description+"|"+str(stop.latitude)+"|"+str(stop.longitude)
-print "----------------"
-for stop in SouthKioskList:
-    print stop.description+"|"+str(stop.latitude)+"|"+str(stop.longitude)
+
 
 
 
