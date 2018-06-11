c1859f63820f2136d8e89dacd5cd6fbd108ec2f7
rreneker <rreneker@umich.edu>
2018-06-10 20:02:34 -0400

    updated depot address to include city and state

--- findPaths.py
+++ findPaths.py
@@ -53,7 +53,7 @@ def TwoOpt(KioskList):
 KioskList = []
 
 
-depot = Kiosk("Starting Depo","1376 W Lake Street" ,41.8851024,-87.6618988)
+depot = Kiosk("Starting Depo","1376 W Lake Street Chicago, IL" ,41.8851024,-87.6618988)
 
 with open('KioskCoords.csv','rb') as csvfile:
     reader = csv.reader(csvfile,delimiter=',',quotechar='"')
